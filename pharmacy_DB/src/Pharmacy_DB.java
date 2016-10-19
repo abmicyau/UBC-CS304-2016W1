f34d40031be56b96290beffd530248b6416de47e
vyangit <vyan.git@gmail.com>
2016-10-18 21:59:23 -0700

    hardcoded a query print out.

--- pharmacy_DB/src/Pharmacy_DB.java
+++ pharmacy_DB/src/Pharmacy_DB.java
@@ -10,6 +10,20 @@ public class Pharmacy_DB {
                     "jdbc:oracle:thin:@localhost:1522:ug", "ora_y3v9a", "a33732132");
             System.out.println("Connected to Oracle Database");
             Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM vaccination");
+
+        	String service_id;
+        	String vaccination_id;
+        	String date_vaccinated;
+        	String dose;
+            
+            while(rs.next()){
+            	service_id = rs.getString("service_id");
+            	vaccination_id = rs.getString("vaccination_id");
+            	date_vaccinated = rs.getString("date_vaccinated");
+            	dose = rs.getString("dose");
+            	System.out.println("{" + service_id + ", " + vaccination_id + ", " + date_vaccinated + ", " + dose + "}");
+            }
         }
         catch (SQLException e) {
             System.err.println("Connection Failed");
