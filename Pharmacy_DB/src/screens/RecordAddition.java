755844ba101f0df2a848639e84b2de8575659105
vyangit <vyan.git@gmail.com>
2016-11-17 23:26:11 -0800

    Record addition and deletion added. Deletion finished.

---
+++ Pharmacy_DB/src/screens/RecordAddition.java
@@ -0,0 +1,64 @@
+package screens;
+
+import main.Pharmacy_DB;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Victor on 2016-11-16.
+ */
+public class RecordAddition extends JPanel{
+    private JButton buttonAdd = new JButton("Add");
+    private JButton buttonBack = new JButton("Back");
+
+    private JPanel messageContainer = new JPanel(new GridLayout(1, 1));
+    private JLabel additionMessage = new JLabel("Adding...");
+
+    private GridBagConstraints constraints = new GridBagConstraints();
+
+    private JPanel left = new JPanel(new GridBagLayout());;
+    private JPanel right = new JPanel(new BorderLayout());;
+
+    public RecordAddition(){
+
+        // important! call JPanel constructor and pass GridBagLayout
+        super(new GridBagLayout());
+
+        // set contraints and padding
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(10, 10, 10, 10);
+        constraints.fill = GridBagConstraints.VERTICAL;
+        constraints.weightx = 0;
+
+        constraints.gridx = 0;
+        add(left, constraints);
+
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.gridx = 1;
+        constraints.weightx = 1;
+        add(right, constraints);
+        constraints.gridheight = 1;
+
+        constraints.fill = GridBagConstraints.NONE;
+
+        // Sets up buttons
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        left.add(buttonAdd, constraints);
+
+        constraints.gridy = 3;
+        left.add(buttonBack, constraints);
+    }
+
+    private class BackButton implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            Pharmacy_DB.switchScreen(Pharmacy_DB.getHomePanel());
+        }
+    }
+
+}
