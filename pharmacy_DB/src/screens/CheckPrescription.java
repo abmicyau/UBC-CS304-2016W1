5dc2a691996c8f3ec842be4ef9a363c6f4534d47
williamtsuii <william_xu@live.com>
2016-11-21 17:31:38 -0800

    delete a helper function i didn't need and fixed a little popup bug

--- pharmacy_DB/src/screens/CheckPrescription.java
+++ pharmacy_DB/src/screens/CheckPrescription.java
@@ -1,5 +1,6 @@
 package screens;
 
+import javafx.beans.property.adapter.JavaBeanObjectProperty;
 import main.Pharmacy_DB;
 import models.DBScreen;
 import models.DBTableModel;
@@ -18,7 +19,7 @@ import static javax.swing.JOptionPane.YES_OPTION;
 
 public class CheckPrescription extends DBScreen {
 
-    private JLabel labelDID = new JLabel("Docto ID: ");
+    private JLabel labelDID = new JLabel("Doctor ID: ");
     private JTextField textDID = new JTextField(10);
     private JLabel labelPID = new JLabel("Customer ID: ");
     private JTextField textPID = new JTextField(10);
@@ -160,7 +161,7 @@ public class CheckPrescription extends DBScreen {
         fillTable(model, Pharmacy_DB.getResults(query.toString()));
 
         if (model.getRowCount() == 0) {
-            JOptionPane.showMessageDialog(warningFrame, "No such prescription exists in the database");
+            JOptionPane.showMessageDialog(warningFrame, "No such prescription exists in the database", "Error", JOptionPane.ERROR_MESSAGE);
         }
 
 
@@ -174,15 +175,16 @@ public class CheckPrescription extends DBScreen {
 
             if ((textPID.getText().length() == 0 || textDID.getText().length() == 0) ||
                     (textPID.getText().matches("[0-9]+") == false) || (textDID.getText().matches("[0-9]+") == false)) {
-                JOptionPane.showMessageDialog(warningFrame, "Please enter a valid Doctor ID and/or Prescription ID.");
+                JOptionPane.showMessageDialog(warningFrame, "Please enter a valid Doctor ID and/or Prescription ID.", "Error", JOptionPane.ERROR_MESSAGE);
 
+            } else {
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        update();
+                    }
+                });
             }
-            SwingUtilities.invokeLater(new Runnable() {
-                @Override
-                public void run() {
-                    update();
-                }
-            });
         }
     }
 
@@ -199,109 +201,6 @@ public class CheckPrescription extends DBScreen {
         }
     }
 
-    private class DetailsDialog extends JDialog implements ActionListener {
 
-        private JPanel dialogPanel = new JPanel(new GridBagLayout());
-        private JButton closeButton = new JButton("Close");
-
-        // labels
-        private JLabel label1 = new JLabel("<html><b>Basic Information</b></html>");
-        private JLabel label1_1 = new JLabel("Employee ID: ");
-        private JLabel label1_2 = new JLabel("Name: ");
-        private JLabel label1_3 = new JLabel("Email: ");
-        private JLabel label1_4 = new JLabel("Phone #: ");
-        private JLabel label1_5 = new JLabel("Address: ");
-        private JLabel label2 = new JLabel("<html><b>Additional Information</b></html>");
-        private JLabel label2_1 = new JLabel("Position: ");
-        private JLabel label2_2 = new JLabel("License #: ");
-
-        // info
-        private JLabel info1_1 = new JLabel("");
-        private JLabel info1_2 = new JLabel("");
-        private JLabel info1_3 = new JLabel("");
-        private JLabel info1_4 = new JLabel("");
-        private JLabel info1_5 = new JLabel("");
-        private JLabel info2_1 = new JLabel("");
-        private JLabel info2_2 = new JLabel("");
-
-        public DetailsDialog() {
-
-            setTitle("Employee Details");
-            constraints.anchor = GridBagConstraints.WEST;
-            // TOP, LEFT, BOTTOM, RIGHT
-            constraints.insets = new Insets(10, 10, 5, 10);
-            constraints.gridwidth = 2;
-
-            constraints.gridx = 0;
-            constraints.gridy = 0;
-            dialogPanel.add(label1, constraints);
-
-            constraints.gridwidth = 1;
-            constraints.insets.set(5, 20, 5, 10);
-            constraints.gridy = 1;
-            dialogPanel.add(label1_1, constraints);
-            constraints.gridx = 1;
-            dialogPanel.add(info1_1, constraints);
-
-            constraints.gridy = 2;
-            constraints.gridx = 0;
-            dialogPanel.add(label1_2, constraints);
-            constraints.gridx = 1;
-            dialogPanel.add(info1_2, constraints);
-
-            constraints.gridy = 3;
-            constraints.gridx = 0;
-            dialogPanel.add(label1_3, constraints);
-            constraints.gridx = 1;
-            dialogPanel.add(info1_3, constraints);
-
-            constraints.gridy = 4;
-            constraints.gridx = 0;
-            dialogPanel.add(label1_4, constraints);
-            constraints.gridx = 1;
-            dialogPanel.add(info1_4, constraints);
-
-            constraints.gridy = 5;
-            constraints.gridx = 0;
-            dialogPanel.add(label1_5, constraints);
-            constraints.gridx = 1;
-            dialogPanel.add(info1_5, constraints);
-
-            constraints.gridwidth = 2;
-            constraints.insets.set(5, 10, 5, 10);
-            constraints.gridx = 0;
-            constraints.gridy = 6;
-            dialogPanel.add(label2, constraints);
-
-            constraints.gridwidth = 1;
-            constraints.insets.set(5, 20, 5, 10);
-            constraints.gridy = 7;
-            dialogPanel.add(label2_1, constraints);
-            constraints.gridx = 1;
-            dialogPanel.add(info2_1, constraints);
-
-            constraints.gridy = 8;
-            constraints.gridx = 0;
-            dialogPanel.add(label2_2, constraints);
-            constraints.gridx = 1;
-            dialogPanel.add(info2_2, constraints);
-
-            constraints.insets.set(15, 10, 10, 10);
-            constraints.gridx = 0;
-            constraints.gridy = 9;
-            dialogPanel.add(closeButton, constraints);
-
-            closeButton.addActionListener(this);
-
-            setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-            setContentPane(dialogPanel);
-
-        }
-
-
-        public void actionPerformed(ActionEvent e) {
-            dispose();
-        }
-    }
 
 }
